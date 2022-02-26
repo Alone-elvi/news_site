cd576db63bfa94bad238a708cd38be244cde57bb
Alone-elvi <alone.elvi@gmail.com>
2022-02-27 02:22:14 +0300

    Laravel news_site first commit

---
+++ news_site/database/factories/ArticleFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Psy\Util\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Article::class;
+    public function definition()
+    {
+        $title = $this->faker->sentence(6, true);
+        $slug = Str::substr(Str::lower(preg_replace('/\s+/', "-", $title)), 0, -1);
+        return [
+            'title' => $title,
+            'body' => $this->faker->paragraph(100, true),
+            'slug' => $slug,
+            'img' => 'https://via.placeholder.com/688/5F1130/FFFFFF/?text=LARAVEL:8.*',
+            'created_at' => $this->faker->dateTimeBetween('-1 years'),
+            //
+        ];
+    }
+}
